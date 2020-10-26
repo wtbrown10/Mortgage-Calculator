a9d8dc8b3ea3efc073a6983aad799efb78116d79
Will brown <wtbrown10@gmail.com>
2020-10-26 17:52:26 -0400

    refractor

---
+++ src/com/will/MortgageReport.java
@@ -0,0 +1,30 @@
+package com.will;
+
+import java.text.NumberFormat;
+
+public class MortgageReport {
+
+    private MortgageCalculator calculator;
+
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (double balance: calculator.getRemainingBalances())
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+    }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+}
